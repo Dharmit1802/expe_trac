bab96a2487381dd9ad4658c407afad19ce016b87
Dharmit1802 <dharmit1802@gmail.com>
2024-03-05 13:56:02 +0530

    d8

--- expencetracker/src/components/CategoryFilter.js
+++ expencetracker/src/components/CategoryFilter.js
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-export default function CategoryFilter({CATEGORIES}) {
+export default function CategoryFilter({ CATEGORIES, handleFilterCategory ,setcategoryflag}) {
+
+    function handleFilter(category) {
+      return()=>{
+
+     
+              handleFilterCategory(category); // You might want to set the filter category here
+          console.log(category);
+      }
+    };
+
     return (
-        <aside >
-          <ul className='fix sticky top-[160px]' > 
-            <li className="category">
-              <button className="btn btn-all-categories ">All</button>
-            </li>
-    
-            {CATEGORIES.map((e) => (
-              <li key={e.name} className="category ">
-                <button className='btn btn-category rounded-full px-4 py-2'
-                  style={{ backgroundColor: e.color }}
-                  >
-                  {e.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </aside >
-      )
+        <aside>
+            <ul className='fix sticky top-[160px]'>
+                <li className="category">
+                    <button className="btn btn-all-categories" onClick={handleFilter('')}>All</button>
+                </li>
+
+                {CATEGORIES.map((e) => (
+                    <li key={e.name} className="category">
+                        <button
+                            className='btn btn-category rounded-full px-4 py-2'
+                            style={{ backgroundColor: e.color }}
+                            onClick={handleFilter(e.name)}
+                        >
+                            {e.name}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </aside>
+    );
 }
